af0bd4d1f938aab2aab03271c65850d903fa1c53
mariamoratalla <maria.moratalla97@gmail.com>
2024-03-08 20:17:27 +0100

    barajar cartas

--- 1-barajar-cartas/src/main.ts
+++ 1-barajar-cartas/src/main.ts
@@ -1,3 +1,12 @@
-import "./style.css";
+function shuffleArray<T>(array: T[]): T[] {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
-console.log("Hello Typescript!");
+const myArray = [1, 2, 3, 4, 5];
+const shuffledArray = shuffleArray(myArray);
+console.log(shuffledArray);
+
