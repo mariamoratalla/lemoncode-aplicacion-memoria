7a660ee231fa9ff9dd11160787e534bad39df9a8
mariamoratalla <maria.moratalla97@gmail.com>
2024-03-08 21:00:41 +0100

    voltear carta

--- 2-voltear-cartas/src/main.ts
+++ 2-voltear-cartas/src/main.ts
@@ -1,3 +1,15 @@
-import "./style.css";
+function mostrarImagen(): void {
+  const imagen = document.getElementById("imagen") as HTMLImageElement;
 
-console.log("Hello Typescript!");
+  if (imagen && imagen instanceof HTMLImageElement) {
+    imagen.src =
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png";
+    imagen.alt = "leon";
+  }
+}
+
+const carta = document.querySelector(".imagen-container") as HTMLDivElement;
+
+if (carta && carta instanceof HTMLDivElement) {
+  document.addEventListener("click", mostrarImagen);
+}
