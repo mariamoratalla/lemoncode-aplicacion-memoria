7b1fe98fb144b758a9512113010191e98d659e43
mariamoratalla <maria.moratalla97@gmail.com>
2024-03-10 19:17:07 +0100

    posible modelo

---
+++ src/modelo.ts
@@ -0,0 +1,85 @@
+export interface Carta {
+  idFoto: number;
+  imagen: string;
+  estaVuelta: boolean;
+  encontrada: boolean;
+}
+
+interface InfoCarta {
+  idFoto: number;
+  imagen: string;
+}
+
+const infoCartas: InfoCarta[] = [
+  {
+    idFoto: 1,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png",
+  },
+  {
+    idFoto: 2,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/2.png",
+  },
+  {
+    idFoto: 3,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/3.png",
+  },
+  {
+    idFoto: 4,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/4.png",
+  },
+];
+
+const crearCartaInicial = (idFoto: number, imagen: string): Carta => ({
+  idFoto,
+  imagen,
+  estaVuelta: false,
+  encontrada: false,
+});
+
+/* Aquí crearemos un array de cartas a partir de un array de infoCartas
+    y duplicaremos las cartas para que haya dos de cada tipo. */
+    
+const crearColeccionDeCartasInicial = (infoCartas: InfoCarta[]): Carta[] => {
+  
+  const coleccionCartas: Carta[] = [];
+  infoCartas.forEach((carta) => {
+    coleccionCartas.push(crearCartaInicial(carta.idFoto, carta.imagen));
+    coleccionCartas.push(crearCartaInicial(carta.idFoto, carta.imagen));
+  });
+  return coleccionCartas;
+};
+
+export let cartas: Carta[] = crearColeccionDeCartasInicial(infoCartas);
+
+/*
+    Aquí definimos el tipo de estado de la partida, la idea es que cuando empiece la partida
+    todas las cartas estén boca abajo y si se hacen click sobre ellas no se volteen.
+    EstadoPartida = "PartidaNoIniciada", una vez que se pulse Iniciar partida el estado 
+    de la partida cambiaría a "CeroCartasLevantadas" y así sucesivamente.
+
+  */
+
+type EstadoPartida =
+  | "PartidaNoIniciada"
+  | "CeroCartasLevantadas"
+  | "UnaCartaLevantada"
+  | "DosCartasLevantadas"
+  | "PartidaCompleta";
+
+export interface Tablero {
+  cartas: Carta[];
+  estadoPartida: EstadoPartida;
+  indiceCartaVolteadaA?: number;
+  indiceCartaVolteadaB?: number;
+}
+
+const crearTableroInicial = (): Tablero => ({
+  cartas: cartas,
+  estadoPartida: "PartidaNoIniciada",
+});
+
+export let tablero: Tablero = crearTableroInicial();
